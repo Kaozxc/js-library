184ed76da2e52748684edf8c9fbc8438e90e2014
Kaoz <kaoz415@gmail.com>
2021-12-17 15:39:57 +0100

    implemeting

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 let myLibrary = [
+  {
+    title: "Title",
+    author: "Author",
+    pages: "Pages",
+    read: "Read",
+  },
   {
     title: "The Hobbit",
     author: "Tolkien",
@@ -42,13 +49,13 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  let userInputTitle = prompt("Please provide your favourite book title.");
-  let userInputAuthor = prompt("Please provide your favourite book Author.");
-  let userInputPages = prompt(
-    "Please provide your favourite book number of pages."
-  );
-  let userInputRead = prompt("Please tell if you read the book.");
+  let table = document.querySelector("table");
+  table.innerHTML = "";
 
+  let userInputTitle = prompt("Please provide your favourite book title.");
+  let userInputAuthor = prompt("Please provide Author.");
+  let userInputPages = prompt("Please provide number of pages.");
+  let userInputRead = prompt("Please tell if you read the book.");
   let newBook = {
     title: userInputTitle,
     author: userInputAuthor,
@@ -65,25 +72,65 @@ function displayBook() {
   myLibrary.forEach((item) => console.log(item));
 }
 
+function removeBook() {
+  myLibrary.pop();
+  generate_table();
+}
+
+let removeBtn = document.querySelector("#remove");
+let removeBtn2 = document.querySelector("#remove2");
+let removeBtn3 = document.querySelector("#remove3");
+let removeBtn4 = document.querySelector("#remove4");
+let removeBtn5 = document.querySelector("#remove5");
+let removeBtn6 = document.querySelector("#remove6");
+
+removeBtn.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+removeBtn2.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+removeBtn3.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+removeBtn4.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+removeBtn5.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+removeBtn6.addEventListener("click", () => {
+  myLibrary.pop();
+  generate_table();
+});
+
 function generate_table() {
   // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
+  let body = document.getElementsByTagName("body")[0];
 
   // creates a <table> element and a <tbody> element
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  let tbl = document.createElement("table");
+  let tblBody = document.createElement("tbody");
 
   // creating all cells
   for (var i = 0; i < myLibrary.length; i++) {
     // creates a table row
-    var row = document.createElement("tr");
-
-    for (var j = 0; j < 4; j++) {
+    let row = document.createElement("tr");
+    for (var j = 0; j < 5; j++) {
       // Create a <td> element and a text node, make the text
       // node the contents of the <td>, and put the <td> at
       // the end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(
+      let cell = document.createElement("td");
+      let cellTextTitle = document.createTextNode(myLibrary[i].title);
+      let cellTextAuthor = document.createTextNode(myLibrary[i].author);
+      let cellTextPages = document.createTextNode(myLibrary[i].pages);
+      let cellTextRead = document.createTextNode(myLibrary[i].read);
+      let cellText = document.createTextNode(
         myLibrary[i].title +
           "," +
           myLibrary[i].author +
@@ -92,14 +139,38 @@ function generate_table() {
           "," +
           myLibrary[i].read
       );
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+      if (j === 0) {
+        cell.appendChild(cellTextTitle);
+        row.appendChild(cell);
+      } else if (j === 1) {
+        cell.appendChild(cellTextAuthor);
+        row.appendChild(cell);
+      } else if (j === 2) {
+        cell.appendChild(cellTextPages);
+        row.appendChild(cell);
+      } else if (j === 3) {
+        cell.appendChild(cellTextRead);
+        row.appendChild(cell);
+      }
+      if (i === 1) {
+        row.appendChild(removeBtn);
+      } else if (i === 2) {
+        row.appendChild(removeBtn2);
+      } else if (i === 3) {
+        row.appendChild(removeBtn3);
+      } else if (i === 4) {
+        row.appendChild(removeBtn4);
+      } else if (i === 5) {
+        row.appendChild(removeBtn5);
+      } else if (i === 0) {
+      } else {
+        row.appendChild(removeBtn6);
+      }
     }
 
     // add the row to the end of the table body
     tblBody.appendChild(row);
   }
-
   // put the <tbody> in the <table>
   tbl.appendChild(tblBody);
   // appends <table> into <body>
@@ -110,4 +181,14 @@ function generate_table() {
 
 generate_table();
 
-// "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+// if (i === 0) {
+//   row.appendChild(removeBtn);
+// } else if (i === 1) {
+//   row.appendChild(removeBtn);
+// } else if (i === 2) {
+//   row.appendChild(removeBtn);
+// } else if (i === 3) {
+//   row.appendChild(removeBtn);
+// } else {
+//   row.appendChild(removeBtn);
+// }
